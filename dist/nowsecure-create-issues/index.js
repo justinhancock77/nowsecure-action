b9fb135dab5c37f4beae42e4bde9f76dd7fb34f9
justinhancock77 <jjhancock@gmail.com>
2022-07-06 10:05:05 -0600

    clean up and run the whole workflow end to end

--- dist/nowsecure-create-issues/index.js
+++ dist/nowsecure-create-issues/index.js
@@ -59162,58 +59162,55 @@ const sleep = (0, util_1.promisify)(setTimeout);
 // unique to determine whether the GH issue exists already
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
-        const octokit = new action_1.Octokit({
-            auth: core.getInput("GITHUB_TOKEN"),
-        });
-        console.log("octokit loaded");
-        const apiUrl = core.getInput("api_url");
-        const labApiUrl = core.getInput("lab_api_url");
-        const platformToken = core.getInput("platform-token");
-        const reportId = core.getInput("report_id");
-        console.log("fetch report with id", reportId);
-        const ns = new nowsecure_client_1.NowSecure(platformToken, apiUrl, labApiUrl);
-        let pollInterval = 60000;
-        // Poll Platform to resolve the report ID to a report.
-        // GitHub Actions will handle the timeout for us in the event something goes awry.
-        let report = null;
-        for (;;) {
-            console.log("Checking for NowSecure report... ", reportId);
-            report = yield ns.pullReport(reportId);
-            console.log("report?", report);
-            // NOTE: No optional chaining on Node.js 12 in GitHub Actions.
-            try {
-                if (report.data.auto.assessments[0].report) {
-                    console.log("found the report");
-                    break;
-                }
-                else {
-                    console.log("sleep");
-                    yield sleep(pollInterval);
-                }
-            }
-            catch (e) {
-                console.error(e);
-                // No report data.
-            }
-        }
-        // console.log(
-        //   "report!!",
-        //   JSON.stringify(report.data.auto.assessments[0].report)
-        // );
-        for (var resp of report.data.auto.assessments[0].report.findings) {
-            console.log("resp", resp);
-            // should I break this out into a github-client.ts utility?
-            yield octokit.request("POST /repos/{owner}/{repo}/issues", {
-                owner: "justinhancock77",
-                repo: "nowsecure-action",
-                title: resp.title,
-                body: resp.summary,
-                assignees: ["justinhancock77"],
-                // milestone: 1,
-                labels: ["bug"],
+        // check to see if enable_issues is true
+        if (core.getInput("enable_issues")) {
+            const octokit = new action_1.Octokit({
+                auth: core.getInput("GITHUB_TOKEN"),
             });
+            console.log("octokit loaded");
+            const apiUrl = core.getInput("api_url");
+            const labApiUrl = core.getInput("lab_api_url");
+            const platformToken = core.getInput("platform-token");
+            const reportId = core.getInput("report_id");
+            console.log("fetch report with id", reportId);
+            const ns = new nowsecure_client_1.NowSecure(platformToken, apiUrl, labApiUrl);
+            let pollInterval = 60000;
+            // Poll Platform to resolve the report ID to a report.
+            // GitHub Actions will handle the timeout for us in the event something goes awry.
+            let report = null;
+            for (;;) {
+                console.log("Fetch report:", reportId);
+                report = yield ns.pullReport(reportId);
+                // NOTE: No optional chaining on Node.js 12 in GitHub Actions.
+                try {
+                    if (report.data.auto.assessments[0].report) {
+                        console.log("report found");
+                        break;
+                    }
+                    else {
+                        console.log("sleep");
+                        yield sleep(pollInterval);
+                    }
+                }
+                catch (e) {
+                    console.error(e);
+                    // No report data.
+                }
+            }
+            for (var resp of report.data.auto.assessments[0].report.findings) {
+                console.log("resp", resp);
+                // should we break this out into a github-client.ts utility?
+                yield octokit.request("POST /repos/{owner}/{repo}/issues", {
+                    owner: "justinhancock77",
+                    repo: "nowsecure-action",
+                    title: resp.title,
+                    body: resp.summary,
+                    assignees: ["justinhancock77"],
+                    // milestone: 1,
+                    labels: ["bug"], // enum here
+                });
+            }
         }
-        //console.log("Hello, %s", data);
     });
 }
 exports.run = run;
--- src/nowsecure-create-issues.ts
+++ src/nowsecure-create-issues.ts
@@ -16,62 +16,59 @@ const sleep = promisify(setTimeout);
 // WITHOUT duplicating issues on each run.  Need to use the hash / something
 // unique to determine whether the GH issue exists already
 export async function run() {
-  const octokit = new Octokit({
-    auth: core.getInput("GITHUB_TOKEN"),
-  });
-  console.log("octokit loaded");
+  // check to see if enable_issues is true
+  if (core.getInput("enable_issues")) {
+    const octokit = new Octokit({
+      auth: core.getInput("GITHUB_TOKEN"),
+    });
+    console.log("octokit loaded");
 
-  const apiUrl = core.getInput("api_url");
-  const labApiUrl = core.getInput("lab_api_url");
+    const apiUrl = core.getInput("api_url");
+    const labApiUrl = core.getInput("lab_api_url");
 
-  const platformToken = core.getInput("platform-token");
-  const reportId = core.getInput("report_id");
-  console.log("fetch report with id", reportId);
+    const platformToken = core.getInput("platform-token");
+    const reportId = core.getInput("report_id");
+    console.log("fetch report with id", reportId);
 
-  const ns = new NowSecure(platformToken, apiUrl, labApiUrl);
-  let pollInterval = 60000;
+    const ns = new NowSecure(platformToken, apiUrl, labApiUrl);
+    let pollInterval = 60000;
 
-  // Poll Platform to resolve the report ID to a report.
-  // GitHub Actions will handle the timeout for us in the event something goes awry.
-  let report = null;
-  for (;;) {
-    console.log("Checking for NowSecure report... ", reportId);
-    report = await ns.pullReport(reportId);
-    console.log("report?", report);
-    // NOTE: No optional chaining on Node.js 12 in GitHub Actions.
-    try {
-      if (report.data.auto.assessments[0].report) {
-        console.log("found the report");
-        break;
-      } else {
-        console.log("sleep");
-        await sleep(pollInterval);
+    // Poll Platform to resolve the report ID to a report.
+    // GitHub Actions will handle the timeout for us in the event something goes awry.
+    let report = null;
+    for (;;) {
+      console.log("Fetch report:", reportId);
+      report = await ns.pullReport(reportId);
+
+      // NOTE: No optional chaining on Node.js 12 in GitHub Actions.
+      try {
+        if (report.data.auto.assessments[0].report) {
+          console.log("report found");
+          break;
+        } else {
+          console.log("sleep");
+          await sleep(pollInterval);
+        }
+      } catch (e) {
+        console.error(e);
+        // No report data.
       }
-    } catch (e) {
-      console.error(e);
-      // No report data.
+    }
+
+    for (var resp of report.data.auto.assessments[0].report.findings) {
+      console.log("resp", resp);
+      // should we break this out into a github-client.ts utility?
+      await octokit.request("POST /repos/{owner}/{repo}/issues", {
+        owner: "justinhancock77",
+        repo: "nowsecure-action",
+        title: resp.title,
+        body: resp.summary,
+        assignees: ["justinhancock77"],
+        // milestone: 1,
+        labels: ["bug"], // enum here
+      });
     }
   }
-
-  // console.log(
-  //   "report!!",
-  //   JSON.stringify(report.data.auto.assessments[0].report)
-  // );
-
-  for (var resp of report.data.auto.assessments[0].report.findings) {
-    console.log("resp", resp);
-    // should I break this out into a github-client.ts utility?
-    await octokit.request("POST /repos/{owner}/{repo}/issues", {
-      owner: "justinhancock77",
-      repo: "nowsecure-action",
-      title: resp.title,
-      body: resp.summary,
-      assignees: ["justinhancock77"],
-      // milestone: 1,
-      labels: ["bug"],
-    });
-  }
-  //console.log("Hello, %s", data);
 }
 
 run();
