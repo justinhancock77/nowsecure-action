61366bb38b9424027ba6ee6880bd647244eedcf3
justinhancock77 <jjhancock@gmail.com>
2022-08-02 10:31:21 -0400

    check for body before indexOf

--- src/nowsecure-create-issues.ts
+++ src/nowsecure-create-issues.ts
@@ -114,7 +114,11 @@ export async function issueExists(finding: Finding, existing: any) {
   // pass back -1 to do nothing (we already have this issue, and it's not closed)
   let result = 0; // default to we didn't find THIS issue in the existing collection
   for (var ex of existing) {
-    if (ex.title === finding.title && ex.body.indexOf(finding.key) >= 0) {
+    if (
+      ex.title === finding.title &&
+      ex.body &&
+      ex.body.indexOf(finding.key) >= 0
+    ) {
       // unique key matches
       // the issue already exists, check status
       console.log("Issue title and unique_id match");
