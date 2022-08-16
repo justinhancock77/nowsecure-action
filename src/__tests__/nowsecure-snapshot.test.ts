7426fab74f9b42c4746b74e6650764868ff3ffa5
justinhancock77 <jjhancock@gmail.com>
2022-08-16 13:14:53 -0600

    checkpoint for keegan to try locally to see if timeout occurs for him as well

--- src/__tests__/nowsecure-snapshot.test.ts
+++ src/__tests__/nowsecure-snapshot.test.ts
@@ -10,17 +10,14 @@ import { promises } from "fs";
 import { convertToSnapshot, submitSnapshotData } from "../nowsecure-snapshot";
 import type { Deputy } from "../types/deputy";
 import path from "path";
-
 import nock from "nock";
-import { DependencySnapshot } from "../types/dependency-snapshot";
-// import { DEFAULT_API_URL } from "../nowsecure-client";
-// import nock from "nock";
-// import { clear } from "console";
+import { USER_AGENT } from "../nowsecure-client";
+import * as client from "@actions/http-client";
 
 const { readFile } = promises;
 
 jest.useFakeTimers().setSystemTime(new Date("2000-01-01"));
-jest.setTimeout(60000);
+jest.setTimeout(30000);
 
 describe("Snapshot conversion", () => {
   const context = {
@@ -44,18 +41,30 @@ describe("Snapshot conversion", () => {
   });
 
   test("can fail with max retry attempts", async () => {
-    const data = await readFile(
-      path.join(__dirname, "resources", "deputy.json")
-    );
-    const parsed = JSON.parse(data.toString());
-    const snapshot = convertToSnapshot(parsed as Deputy, "", context);
-    // how do I mock this to then run expect / assertions on the number
-    // of times it ran the retry?
-    //const spy = jest.spyOn(nowsecure-snapshot, 'submitSnapshotData');
+    // const data = await readFile(
+    //   path.join(__dirname, "resources", "deputy.json")
+    // );
+    // const parsed = JSON.parse(data.toString());
+    // const snapshot = convertToSnapshot(parsed as Deputy, "", context);
+
+    const scope = nock("https://example.com/").get("/").reply(502);
+    const httpClient = new client.HttpClient();
+    const r = await httpClient.get("https://example.com/");
+    expect(r.message.statusCode).toBe(502);
+    // const r = await httpClient.post(
+    //   //snapshotUrl(owner, repo),
+    //   "https://example.com/repos/justinhancock77/nowsecure-action/dependency-graph/snapshots",
+    //   "{}"
+    //   // {
+    //   //   Authorization: `token aabbccdd`,
+    //   // }
+    // );
+
+    
 
     //  in real life, when submitSnapshotData is called, we will reference the GH token
-    await submitSnapshotData(snapshot, context, "abc123").then((data) => {
-      console.log("data", data);
-    });
+    // await submitSnapshotData(snapshot, context, "abc123").then((data) => {
+    //   console.log("data", data);
+    // });
   });
 });
